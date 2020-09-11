69e8374b1a5a3da6ba1b3ab2925012e8f319c179
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-11 04:53:40 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,5 @@
-first_number = 2 
-second_number = second_number.to_f
+first_number = 4
+second_number = 2
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
