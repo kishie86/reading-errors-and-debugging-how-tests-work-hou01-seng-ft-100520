a0aa0b9bebb4219eb993e1c28e21a9ff3366cefe
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-11 04:42:54 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+first_number = first_number.to_i
+second_number = second_number.to_f
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
